5a6d52b413c459e2aeea7cc1f33c1acbc69014b5
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-06-23 15:16:39 +0530

    style the active link in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,30 +1,40 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { SiPivotaltracker } from "react-icons/si";
+import classnames from 'classnames';
 
-const Navbar = () => {
+const Navbar = () => {  
 
-  const links = [
-    {
-      name: 'Dashboard',
-      href: '/'
-    },
-    {
-      name: 'Issues',
-      href: '/issues'
-    }
-  ]  
+    const currentPath = usePathname();
 
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><SiPivotaltracker /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link 
-            key={link.href} 
-            className='text-zinc-500 hover:text-zinc-900 transition-colors' 
-            href={link.href}>{link.name}</Link>)}
-        </ul>
-    </nav>
+    const links = [
+        {
+        name: 'Dashboard',
+        href: '/'
+        },
+        {
+        name: 'Issues',
+        href: '/issues'
+        }
+    ]  
+
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/"><SiPivotaltracker /></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => <Link 
+                key={link.href} 
+                className={classnames({
+                    'text-zinc-900' : link.href === currentPath,
+                    'text-zinc-500' : link.href !== currentPath,
+                    'hover:text-zinc-900 transition-colors' : true
+                })}
+                href={link.href}>{link.name}</Link>)}
+            </ul>
+        </nav>
   )
 }
 
